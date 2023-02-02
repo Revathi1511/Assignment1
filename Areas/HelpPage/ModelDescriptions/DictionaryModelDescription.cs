55e1c17b300d9ff8c4d1f14ee3a1a3e4febd53b0
Revathi Ravi <Revathi Ravi@LAPTOP-2GSAKSRF>
2023-02-01 22:03:42 -0500

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Assignmentweek1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Controllers/AddtenController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignmentweek1.Controllers
+{
+    public class AddtenController : ApiController
+    {
+        // GET api/AddTen/{id}
+        public IEnumerable<int> Get(int id)
+        {
+            return new int[] { id + 10 };
+        }
+    }
+}
