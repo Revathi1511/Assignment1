e52e0daa4dafcc1d064adbdb90dd0d6413e5e172
Revathi1511 <n01581051@humber.ca>
2023-02-02 20:29:45 -0500

    Added a new controller

---
+++ Controllers/SquareController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignmentweek1.Controllers
+{
+    public class SquareController : ApiController
+    {
+        // GET api/Square/{id}
+        public IEnumerable<int> Get(int id)
+        {
+            double result = Math.Pow(id, 2);
+            return new int[] { (int)result };
+        }
+
+    }
+}
