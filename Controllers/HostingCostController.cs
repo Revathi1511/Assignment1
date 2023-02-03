370c08f18dfec402c1baf0c682ad95d3696ced28
Revathi1511 <n01581051@humber.ca>
2023-02-02 21:43:14 -0500

    Initiative question

---
+++ Controllers/HostingCostController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignmentweek1.Controllers
+{
+    public class HostingCostController : ApiController
+    {
+        //api/HostingCost/{id} 
+        public string Get(int id) {
+            int noOffortlight = (id / 34) + 1;
+            double chargessFN = 5.50 * noOffortlight;
+            double percentage = ((chargessFN / 100) * 13);
+            double totalCost = chargessFN + percentage;
+
+            return $"{noOffortlight} fortnights at $5.50/FN= ${chargessFN} CAD \n" + $" HST13 %= ${percentage} CAD \n" + $"Total = ${totalCost} CAD";
+        }
+    }
+}
